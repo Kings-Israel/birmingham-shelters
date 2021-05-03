ee86bc077cdaf6cb0c5017f486a5a75c10f9cf69
steekam <waynewanyee@gmail.com>
2021-05-03 16:22:21 +0300

    init: created layout file based on the html template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        @if($pageTitle)
+        {{ $pageTitle }} | {{ config('app.name') }}
+        @else
+        {{ config('app.name') }}
+        @endif
+    </title>
+
+    <!-- Template Theme CSS -->
+    <link href="assets/css/styles.css" rel="stylesheet">
+
+    <!-- Custom Color Option -->
+    <link href="assets/css/colors.css" rel="stylesheet">
+
+    @stack('styles')
+</head>
+
+<body class="blue-skin">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div id="preloader">
+        <div class="preloader"><span></span><span></span></div>
+    </div>
+
+    <div id="main-wrapper">
+        @include('partials.navigation')
+
+        {{ $slot }}
+
+        @include('partials.footer')
+
+        <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+    </div>
+
+    @stack('modals')
+
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/rangeslider.js"></script>
+    <script src="assets/js/select2.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/slider-bg.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/imagesloaded.js"></script>
+
+    <script src="assets/js/custom.js"></script>
+
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,29 @@
+<div class="header header-light head-shadow">
+    <div class="container">
+        <nav id="navigation" class="navigation navigation-landscape">
+            <div class="nav-header">
+                <a class="nav-brand" href="#">
+                    <img src="assets/img/logo.png" class="logo" alt="" />
+                </a>
+                <div class="nav-toggle"></div>
+            </div>
+            <div class="nav-menus-wrapper" style="transition-property: none;">
+                <ul class="nav-menu">
+                    {{-- Main Navigation --}}
+
+                </ul>
+
+                <ul class="nav-menu nav-menu-social align-to-right">
+                    <li><a href="JavaScript:Void(0);" data-bs-toggle="modal" data-bs-target="#signup">Sign Up</a></li>
+
+                    <li class="add-listing">
+                        <a href="JavaScript:Void(0);" data-bs-toggle="modal" data-bs-target="#login"><img
+                                src="assets/img/user-light.svg" width="12" alt="" class="mr-2" />Sign In</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+
+<div class="clearfix"></div>
