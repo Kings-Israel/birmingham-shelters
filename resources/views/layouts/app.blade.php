7c8dcacd806fc3b16e4a506bae8df6d3bd2d0dd9
steekam <waynewanyee@gmail.com>
2021-05-11 12:22:51 +0300

    refactor: changed to using asset() helper for url resolution

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,10 @@
     </title>
 
     <!-- Template Theme CSS -->
-    <link href="/assets/css/styles.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
 
     <!-- Custom Color Option -->
-    <link href="/assets/css/colors.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/colors.css') }}" rel="stylesheet">
 
     @stack('styles')
 </head>
@@ -45,17 +45,17 @@
     <!-- ============================================================== -->
     <!-- All Jquery -->
     <!-- ============================================================== -->
-    <script src="/assets/js/jquery.min.js"></script>
-    <script src="/assets/js/popper.min.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/rangeslider.js"></script>
-    <script src="/assets/js/select2.min.js"></script>
-    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="/assets/js/slick.js"></script>
-    <script src="/assets/js/slider-bg.js"></script>
-    <script src="/assets/js/lightbox.js"></script>
-    <script src="/assets/js/imagesloaded.js"></script>
-    <script src="/assets/js/custom.js"></script>
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/rangeslider.js') }}"></script>
+    <script src="{{ asset('assets/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.js') }}"></script>
+    <script src="{{ asset('assets/js/slider-bg.js') }}"></script>
+    <script src="{{ asset('assets/js/lightbox.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 
     <!-- ============================================================== -->
     <!-- This page plugins -->
