529d630e9a7c174d7ea92314eb1cc2a6c7c8bb77
steekam <waynewanyee@gmail.com>
2021-05-17 09:54:50 +0300

    feat: user update personal information on profile

---
+++ resources/views/livewire/change-password-form.blade.php
@@ -0,0 +1,27 @@
+<div class="dashboard-wraper">
+    <div class="form-submit">
+        <h4>Change Password</h4>
+        <div class="submit-section">
+            <div class="row">
+                <div class="form-group col-md-12">
+                    <label>Old Password</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label>New Password</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label>Confirm New Password</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form-group col-md-12">
+                    <button type="submit" class="btn btn-theme-light-2 float-end">Change Password</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
