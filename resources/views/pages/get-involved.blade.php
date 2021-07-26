6ea9554b124659ca983f249838ccac0520438207
kings-israel <milimokings@gmail.com>
2021-07-26 12:24:03 +0300

    Style changes

--- resources/views/pages/get-involved.blade.php
+++ resources/views/pages/get-involved.blade.php
@@ -2,7 +2,7 @@
     <x-page-title title="Want To Partner With Us?"
         description="Here's a list of ways you can do that" />
         <section class="bg-white">
-            <div class="container">
+            <div class="mx-4">
                 <div class="row">
                     <div class="col-lg-12 col-md-12">
                         <div class="sec-heading center">
@@ -12,31 +12,35 @@
                     </div>
                 </div>
 
-                <div class="row g-3">
-                    <div class="col-md-6 col-lg-6">
-                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full">
+                <div class="row g-3 text-center">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full" id="involved-page">
+                            <img src="{{ asset('assets/img/donor.jpeg') }}" alt="">
                             <h4>Become a donor</h4>
                             <p class="lead">You can assist us in our cause of reducing homelessness by donating to Sheltered Housing Ltd. Your gift directly supports our work to connect vulnerable people with the most suitable accommodation for them.</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-6">
-                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full" id="involved-page">
+                            <img src="{{ asset('assets/img/volunteer.jpeg') }}" alt="">
                             <h4>Become a volunteer</h4>
                             <p class="lead">You can assist us to attain a mission by becoming a volunteer in our organization in handling social work. If you're interested in joining the team, please email <a href="mailto:info@birminghamshelters.co.uk">info@birminghamshelters.co.uk</a>
                             </p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-6">
-                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full" id="involved-page">
+                            <img src="{{ asset('assets/img/advocate.jpeg') }}" alt="">
                             <h4>Become a Birmingham Shelters advocate</h4>
                             <p class="lead">You can help get the word out there about our business and most importantly our social cause. You can find us on Facebook and Twitter. We understand that the more people who know about Sheltered Housing Ltd, the more people we will be able to assist.</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-6">
-                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-white px-5 py-4 shadow rounded-3 h-full" id="involved-page">
+                            <img src="{{ asset('assets/img/general.jpeg') }}" alt="">
                             <h4>Other general ways</h4>
                             <p class="lead">Volunteer in the wider homelessness sector, by finding charities to approach for opportunities.</p>
                         </div>
